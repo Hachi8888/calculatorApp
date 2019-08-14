596214d1581fbd23a2924822a158f71522eafc10
Hachi8888 <h.nana.420@gmail.com>
2019-08-14 14:04:51 +0900

    要素の配置と紐付け完了

--- calculatorApp/ViewController.swift
+++ calculatorApp/ViewController.swift
@@ -10,6 +10,74 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // 計算結果を表示するラベル
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    // Cボタン
+    @IBAction func buttonC(_ sender: Any) {
+    }
+    
+    
+   // ÷ボタン
+    @IBAction func buttonDevided(_ sender: Any) {
+    }
+    
+    // +ボタン
+    @IBAction func buttonSum(_ sender: Any) {
+    }
+    
+    // -ボタン
+    @IBAction func buttonMinus(_ sender: Any) {
+    }
+    
+    // =ボタン
+    @IBAction func buttonEqual(_ sender: Any) {
+    }
+    
+    // 0ボタン
+    @IBAction func button0(_ sender: Any) {
+    }
+    
+    // 1ボタン
+    @IBAction func button1(_ sender: Any) {
+    }
+    
+    // 2ボタン
+    @IBAction func button2(_ sender: Any) {
+    }
+    
+    // 3ボタン
+    @IBAction func button3(_ sender: Any) {
+    }
+    
+    
+    // 4ボタン
+    @IBAction func button4(_ sender: Any) {
+    }
+    
+    // 5ボタン
+    @IBAction func button5(_ sender: Any) {
+    }
+    
+    // 6ボタン
+    @IBAction func button6(_ sender: Any) {
+    }
+    
+    
+    // 7ボタン
+    @IBAction func button7(_ sender: Any) {
+    }
+    
+    // 8ボタン
+    @IBAction func button8(_ sender: Any) {
+    }
+    
+    // 9ボタン
+    @IBAction func button9(_ sender: Any) {
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
