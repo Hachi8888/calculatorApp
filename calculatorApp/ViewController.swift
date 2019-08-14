a241750ce045360d950092187d37ca5db1ff1779
Hachi8888 <h.nana.420@gmail.com>
2019-08-14 17:01:33 +0900

    ボタンの紐付けを一度解除、各ボタンにタグ番号をつけた

--- calculatorApp/ViewController.swift
+++ calculatorApp/ViewController.swift
@@ -6,85 +6,41 @@
 //  Copyright © 2019 N-project. All rights reserved.
 //
 
+// Landscape Left/Right のチェックを外して、画面回転時もレイアウトが変わらないようにした
+
 import UIKit
 
 class ViewController: UIViewController {
-
-    // 計算結果を表示するラベル
-    @IBOutlet weak var resultLabel: UILabel!
+   
+ // 変数一覧
+    // 画面に表示する数字
+    var numberOnScreen: Double = 0
     
-    // Cボタン
-    @IBAction func buttonC(_ sender: Any) {
-    }
+    // 最初に入力した数字
+    var previousNumber: Double = 0
     
-    // ÷ボタン
-    @IBAction func buttonDevided(_ sender: Any) {
-    }
+    // 計算プロセスに進んでいいかの判断値（真偽値）
+    var performingMath: Bool = true
     
-    // ×ボタン
-    @IBAction func buttonTimes(_ sender: Any) {
-    }
-    
-    // +ボタン
-    @IBAction func buttonSum(_ sender: Any) {
-    }
-    
-    // -ボタン
-    @IBAction func buttonMinus(_ sender: Any) {
-    }
-    
-    // =ボタン
-    @IBAction func buttonEqual(_ sender: Any) {
-    }
-    
-    // 0ボタン
-    @IBAction func button0(_ sender: Any) {
-    }
-    
-    // 1ボタン
-    @IBAction func button1(_ sender: Any) {
-    }
-    
-    // 2ボタン
-    @IBAction func button2(_ sender: Any) {
-    }
-    
-    // 3ボタン
-    @IBAction func button3(_ sender: Any) {
-    }
-    
-    
-    // 4ボタン
-    @IBAction func button4(_ sender: Any) {
-    }
-    
-    // 5ボタン
-    @IBAction func button5(_ sender: Any) {
-    }
-    
-    // 6ボタン
-    @IBAction func button6(_ sender: Any) {
-    }
-    
-    
-    // 7ボタン
-    @IBAction func button7(_ sender: Any) {
-    }
-    
-    // 8ボタン
-    @IBAction func button8(_ sender: Any) {
-    }
-    
-    // 9ボタン
-    @IBAction func button9(_ sender: Any) {
-    }
-    
+    // 演算子（+、 -、 ×、 ÷）
+//    var operation
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    // ボタンを押したときの処理
+    @IBAction func tappedButton(_ sender: Any) {
+        
+        
+        
+        
+        
+        
+    }
+    
 
 
 }
